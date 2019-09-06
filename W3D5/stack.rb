f72d03152eab14e291f4df626e94e30d065bbb48
Chanakya <chanakya.valluri@gmail.com>
2019-09-06 09:46:50 -0700

    stack implementation

---
+++ W3D5/stack.rb
@@ -0,0 +1,25 @@
+class Stack
+  def initialize
+    # create ivar to store stack here!
+    @array = []
+  end
+
+  def push(el)
+    # adds an element to the stack
+    @array.push(el)
+    el
+  end
+
+  def pop
+    # removes one element from the stack
+    @array.pop
+  end
+
+  def peek
+    # returns, but doesn't remove, the top element in the stack
+    @array.last
+  end
+
+  private
+  attr_reader :array
+end
